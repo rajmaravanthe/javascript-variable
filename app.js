f34fd85f4c19962344354a9b3064ed7aba210eaf
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-03-14 10:29:28 +0530

    added new fils

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Rajesha Maravanthe" ;
+var age = "28";
+var dateofbirth = "05th sep 1987";
+var placeofbirth = "Kundapura";
+
+console.log("Name: "+ name);
+console.log("Age: "+ age);
+console.log("DOB: "+ dateofbirth);
+console.log("Place: "+ placeofbirth);
